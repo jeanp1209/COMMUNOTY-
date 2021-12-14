4922eff22f5b4fac8e603d947ca80ca843a4d543
BOXEUR <59197730+jeanp1209@users.noreply.github.com>
2021-12-14 07:50:23 -0500

    Initialize upgrade-react-transition-group

---
+++ app/src/main-process/shell.ts
@@ -0,0 +1,32 @@
+import * as Url from 'url'
+import { shell } from 'electron'
+
+/**
+ * Wraps the inbuilt shell.openItem path to address a focus issue that affects macOS.
+ *
+ * When opening a folder in Finder, the window will appear behind the application
+ * window, which may confuse users. As a workaround, we will fallback to using
+ * shell.openExternal for macOS until it can be fixed upstream.
+ *
+ * CAUTION: This method should never be used to open user-provided or derived
+ * paths. It's sole use is to open _directories_ that we know to be safe, no
+ * verification is performed to ensure that the provided path isn't actually
+ * an executable.
+ *
+ * @param path directory to open
+ */
+export function UNSAFE_openDirectory(path: string) {
+  if (__DARWIN__) {
+    const directoryURL = Url.format({
+      pathname: path,
+      protocol: 'file:',
+      slashes: true,
+    })
+
+    shell
+      .openExternal(directoryURL)
+      .catch(err => log.error(`Failed to open directory (${path})`, err))
+  } else {
+    shell.openItem(path)
+  }
+}
